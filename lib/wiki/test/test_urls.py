1c24542a144bd3745f8fd3937acbb1e230a8f143
Nigel Chapman <nigel@chapman.id.au>
2020-04-11 15:15:33 +1000

    New URL handling

---
+++ lib/wiki/test/test_urls.py
@@ -0,0 +1,43 @@
+from .context import lib  # noqa: F401
+
+from lib.wiki.urls import Urls
+from lib.wiki.placeholders import DELIMITER
+
+
+def test_urls():
+    """
+    Test that URLs are matched and restored
+    """
+    url = r"https://example.org"
+    parts = {'test': url}
+    _ = Urls()
+    tokenized = _.insert(parts)
+    expected = {'test': DELIMITER.join([
+        "", "ur:1", ""
+    ])}
+    assert expected == tokenized
+    decorated = _.replace(tokenized)
+    expected = {
+        'test': "<a href=\"%s\" target=\"_blank\">%s</a>" % (url, url)
+    }
+    assert expected == decorated
+
+
+def test_emails():
+    """
+    Test that emails are matched and restored.
+    """
+    email = "hello@example.org"
+    emailto = "mailto:%s" % email
+    parts = {'test': email}
+    _ = Urls()
+    tokenized = _.insert(parts)
+    expected = {'test': DELIMITER.join([
+        "", "ur:1", ""
+    ])}
+    assert expected == tokenized
+    decorated = _.replace(tokenized)
+    expected = {
+        'test': "<a href=\"%s\" target=\"_blank\">%s</a>" % (emailto, email)
+    }
+    assert expected == decorated
---
+++ lib/wiki/urls.py
@@ -0,0 +1,48 @@
+"""
+Article Wiki: URLs
+
+In Article Wiki, URLs are removed before processing so that they don't interact
+strangely with surrounding text, trailing punctuation, or wiki formatting.
+
+NOTE: Unused for now; would require changes to setting and footnote handling.
+"""
+
+import re
+
+from html import escape
+from validators.email import email
+
+from lib.wiki.placeholders import Placeholders
+
+
+class Urls(object):
+    """
+    Extract backslashes characters; restore with backslashes removed.
+    """
+
+    def __init__(self):
+        "This is a thin wrapper for Placeholders"
+        url_chars = re.escape(r'.:\/_+?&=-#%~')
+        regexps = [
+            r'https?://[\w%s]+\w' % url_chars,  # <-- url
+            r'[\w\.\-\_]+@[\w\.\-\_]+\w'  # <-- email
+        ]
+        regexp = '(' + '|'.join(regexps) + ')'
+        self.placeholders = Placeholders(regexp, 'ur')
+
+    def insert(self, parts):
+        "Add placeholders."
+        return self.placeholders.insert(parts)
+
+    def decorate(self, pattern, part_slug=None):
+        "HTML encode the URLs."
+        if email(pattern):
+            link = "mailto:%s" % pattern
+        else:
+            link = pattern
+        html = "<a href=\"%s\" target=\"_blank\">%s</a>"
+        return html % (link, escape(pattern))
+
+    def replace(self, html_parts):
+        "Put the URLs back in the finishes text"
+        return self.placeholders.replace(self.decorate, html_parts)
