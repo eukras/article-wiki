b49a89d6daa46b29aa5ae4145b1aa2bd0fce1987
Nigel Chapman <nigel@chapman.id.au>
2024-03-12 23:57:46 +1100

    Fixed missing token error

--- lib/login.py
+++ lib/login.py
@@ -1,33 +1,31 @@
-from typing import Annotated
 from lib.data import Data, load_env_config
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import HTTPException, Request, status
+
 
 class Login:
     """
     FastApi Dependency Injector
     """
 
-
-    def __init__(
-            self,
-            request: Request,
-        ):
+    def __init__(self, request: Request):
         """
         Return a logged-in user account, or trigger a 401.
         """
         data = Data(load_env_config())
-        token = request.cookies['token']
-        user = data.login_get(token)
-        self.username = user['username'] if user else None
-        self.is_admin = user['is_admin'] if user else None
-
+        token = request.cookies.get('token', '')
+        if token:
+            user = data.login_get(token)
+            self.username = user['username'] if user else None
+            self.is_admin = user['is_admin'] if user else None
+        else:
+            self.username = None
+            self.is_admin = None
 
     def __bool__(self):
         """
         Is there a login?
         """
         return self.username is not None
-
 
     def require(self):
         """
@@ -36,7 +34,6 @@ class Login:
         if not bool(self):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Login required")
-
 
     def require_admin(self):
         """
@@ -47,7 +44,6 @@ class Login:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                                 detail="Admin login required")
 
-
     def controls(self, user_slug: str):
         """
         Check the logged-in user has authority for the specified user.
@@ -57,12 +53,11 @@ class Login:
             self.is_admin == 1,
         ])
 
-
     def require_control(self, user_slug: str):
         """
         If no authority over user_slug, raise 403.
         """
-        self.require()        
+        self.require()
         if not self.controls(user_slug):
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                                 detail=f"Access denied for user '{user_slug}'")
